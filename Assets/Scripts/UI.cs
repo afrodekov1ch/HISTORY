e2a065dbc98105a0913d0ecff98c5c31078b3b4f
Akaha LoKs <132211327+akaha-loks@users.noreply.github.com>
2024-01-31 00:17:05 +0600

    отправка на модерацию

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -6,8 +6,19 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    private int health;
+
+    private void Update()
+    {
+        health = PlayerPrefs.GetInt("health");
+    }
     public void LoadScene(int scene)
     {
         SceneManager.LoadScene(scene);
+    }
+    public void End()
+    {
+        health -= 1;
+        PlayerPrefs.SetInt("health", health);
     }
 }
